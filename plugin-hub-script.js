12154a642a9c2d124fcd63af34bbab4e34bc3624
Gabriel Kanev <contact@mrgkanev.eu>
2024-09-09 22:41:14 +0300

    Update plugin-hub-script.js

--- plugin-hub-script.js
+++ plugin-hub-script.js
@@ -110,29 +110,6 @@ jQuery(document).ready(function ($) {
     );
   }
 
-  $("#show-beta-plugins").on("change", function () {
-    var showBeta = $(this).is(":checked");
-    $.ajax({
-      url: pluginHubAjax.ajax_url,
-      type: "POST",
-      data: {
-        action: "toggle_beta_plugins",
-        nonce: pluginHubAjax.nonce,
-        show_beta: showBeta,
-      },
-      success: function (response) {
-        if (response.success) {
-          location.reload();
-        } else {
-          alert("Failed to update setting: " + response.data);
-        }
-      },
-      error: function () {
-        alert("An error occurred. Please try again.");
-      },
-    });
-  });
-
   function performAction(
     action,
     data,
@@ -242,45 +219,6 @@ jQuery(document).ready(function ($) {
     processNextPlugin();
   }
 
-  function performAction(
-    action,
-    data,
-    button,
-    processingText,
-    successText,
-    failText,
-    callback
-  ) {
-    button.text(processingText);
-    $.ajax({
-      url: pluginHubAjax.ajax_url,
-      type: "POST",
-      data: $.extend(
-        {
-          action: action,
-          nonce: pluginHubAjax.nonce,
-        },
-        data
-      ),
-      success: function (response) {
-        if (response.success) {
-          button.text(successText);
-          showMessage(response.data, "success");
-          if (callback) callback(true);
-        } else {
-          button.text(failText);
-          showMessage(response.data, "error");
-          if (callback) callback(false);
-        }
-      },
-      error: function () {
-        button.text(failText);
-        showMessage("An error occurred. Please try again.", "error");
-        if (callback) callback(false);
-      },
-    });
-  }
-
   function showMessage(message, type) {
     var messageDiv = $("#plugin-hub-messages");
     if (!messageDiv.length) {
@@ -297,91 +235,4 @@ jQuery(document).ready(function ($) {
       messageDiv.fadeOut();
     }, 3000);
   }
-
-  // Install plugin
-  $(".install-now").on("click", function (e) {
-    e.preventDefault();
-    var button = $(this);
-    performAction(
-      "install_github_plugin",
-      { repo: button.data("repo"), url: button.data("url") },
-      button,
-      "Installing...",
-      "Installed",
-      "Install Failed"
-    );
-  });
-
-  // Update plugin
-  $(".update-now").on("click", function (e) {
-    e.preventDefault();
-    var button = $(this);
-    performAction(
-      "update_github_plugin",
-      { repo: button.data("repo"), url: button.data("url") },
-      button,
-      "Updating...",
-      "Updated",
-      "Update Failed"
-    );
-  });
-
-  // Activate plugin
-  $(".activate-now").on("click", function (e) {
-    e.preventDefault();
-    var button = $(this);
-    performAction(
-      "activate_github_plugin",
-      { repo: button.data("repo") },
-      button,
-      "Activating...",
-      "Activated",
-      "Activation Failed"
-    );
-  });
-
-  // Deactivate plugin
-  $(".deactivate-now").on("click", function (e) {
-    e.preventDefault();
-    var button = $(this);
-    performAction(
-      "deactivate_github_plugin",
-      { repo: button.data("repo") },
-      button,
-      "Deactivating...",
-      "Deactivated",
-      "Deactivation Failed"
-    );
-  });
-
-  // Bulk actions
-  $("#plugin-hub-form").on("submit", function (e) {
-    e.preventDefault();
-    var action = $("#bulk-action-selector-top").val();
-    var selectedPlugins = $('input[name="checked[]"]:checked')
-      .map(function () {
-        return $(this).val();
-      })
-      .get();
-
-    if (action === "-1" || selectedPlugins.length === 0) {
-      alert("Please select an action and at least one plugin.");
-      return;
-    }
-
-    switch (action) {
-      case "install":
-        bulkAction("install_github_plugin", selectedPlugins);
-        break;
-      case "activate":
-        bulkAction("activate_github_plugin", selectedPlugins);
-        break;
-      case "deactivate":
-        bulkAction("deactivate_github_plugin", selectedPlugins);
-        break;
-      case "update":
-        bulkAction("update_github_plugin", selectedPlugins);
-        break;
-    }
-  });
 });
